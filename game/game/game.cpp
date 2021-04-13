85234b9f9b677d047ca6621063cfa8c0cf8a4db3
Bamba753 <cheikhahmadoubamba.faye@yahoo.fr>
2021-04-13 17:24:53 +0200

    o

--- game/game/game.cpp
+++ game/game/game.cpp
@@ -7,16 +7,24 @@ Game::Game()
 	this->initWindow();
 	this->initEnemy();
 }
+
 // Destructeurs
 Game::~Game()
 {
 	delete this->window;
 }
+
 // Fonctions privées
 void Game::initVariables()
 {
 	this->window = nullptr;
+	this->points = 0;
+	this->enemySpawnTimerMax = 10.f;
+	this->enemySpawnTimer = this->enemySpawnTimerMax;
+	this->maxEnemies = 5;
+	this->mousehold = false;
 }
+
 void Game::initWindow()
 {
 	this->videoMode.height = 600;
@@ -24,6 +32,7 @@ void Game::initWindow()
 	this->window = new sf::RenderWindow(this->videoMode, "Game", sf::Style::Titlebar | sf::Style::Close);
 	this->window->setFramerateLimit(144);
 }
+
 void Game::initEnemy()
 {
 	this->enemy.setPosition(10.f, 10.f);
@@ -33,24 +42,7 @@ void Game::initEnemy()
 	this->enemy.setOutlineColor(sf::Color::Green);
 	this->enemy.setOutlineThickness(1.f);
 }
-// Fonctions
-void Game::update()
-{
-	this->pollevent();
-	this->enemy.setPosition(sf::Mouse::getPosition(*this->window).x, sf::Mouse::getPosition(*this->window).y);
-	std::cout << "Postion souris :" << sf::Mouse::getPosition(*this->window).x << " " << sf::Mouse::getPosition(*this->window).y << std::endl;
-}
 
-void Game::render()
-{
-	/* - Efface les frames précedents
-	.  - Render les objets
-	.  - Affiche la fénetre
-	*/
-	this->window->clear();
-	this->window->draw(this->enemy);
-	this->window->display();
-}
 
 void Game::pollevent()
 {
@@ -71,8 +63,131 @@ void Game::pollevent()
 	}
 }
 
+void Game::updateMousePosition()
+{
+	/*
+	. return void
+	.
+	. update la position de la souris relative à la fénétre
+	*/
+
+	this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+	this->mousePosView = this->window->mapPixelToCoords(this->mousePosWindow);
+}
+
+void Game::updateEnemies()
+{
+	/*
+	.	return void
+	.	
+	.	Update la liste des enemies 
+	.	remove the enemy at the edge of the screen
+	*/
+	if (this->enemies.size() < this->maxEnemies)
+	{
+		if (this->enemySpawnTimer >= this->enemySpawnTimerMax)
+		{
+			this->spawnEnemies();
+			this->enemySpawnTimer = 0;
+		}
+		else
+			this->enemySpawnTimer += 1.f;
+	}
+
+	bool deleted;
+	for(int i = 0;i < this->enemies.size();i++)
+	{
+		this->enemies[i].move(0.f, 1.0f);
+
+		// Delete if click upon
+		deleted = false;
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			if (this->mousehold == false)
+			{
+				this->mousehold = true;
+				if (this->enemies[i].getGlobalBounds().contains(this->mousePosView))
+				{
+					deleted = true;
+
+					// Gain points
+					this->points += 10;
+				}
+			}
+		}
+		else
+			this->mousehold = false;
+
+
+		std::cout << this->points <<std::endl;
+		// Delete at the edge of the screen
+		if (this->enemies[i].getPosition().y > this->window->getSize().y)
+		{
+			deleted = true;
+		}
+
+		if (deleted)
+			this->enemies.erase(this->enemies.begin() + i);
+	}
+}
+
+
+void Game::spawnEnemies()
+{
+	/*
+	.	return void
+	.
+	.	spawn enemies, modifie la vouleur et la position
+	.	random pos
+	.	random color
+	*/
+	this->enemy.setPosition(
+		static_cast<float>(rand() % static_cast<int>(this->window->getSize().x) - this->enemy.getSize().x),
+		0);
+	
+	this->enemy.setFillColor(sf::Color::Green);
+
+	// Spawn the enemy
+	this->enemies.push_back(this->enemy);
+}
+
 // Acesseurs
 bool Game::running() const
 {
+	// Renvoie l'etat de ma fenetre
 	return (this->window)->isOpen();
+}
+
+// Fonctions
+void Game::update()
+{
+	this->pollevent();
+
+	this->updateMousePosition();
+	this->updateEnemies();
+}
+
+
+
+void Game::renderEnemies()
+{
+	for (auto& i : this->enemies)
+	{
+		this->window->draw(i);
+	}
+}
+
+
+void Game::render()
+{
+	/* - Efface les frames précedents
+	.  - Render les objets
+	.  - Affiche la fénetre
+	*/
+	this->window->clear();
+
+	// Draw objects
+	this->renderEnemies();
+
+	this->window->display();
 }
