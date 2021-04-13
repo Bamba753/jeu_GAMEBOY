a7085116db36e072273d94445397127ebe820c3f
Bamba753 <cheikhahmadoubamba.faye@yahoo.fr>
2021-04-13 16:01:55 +0200

    game

--- game/game/game.cpp
+++ game/game/game.cpp
@@ -1,20 +1,78 @@
-// game.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include "Game.hpp"
 
-#include <iostream>
-
-int main()
+// Constructeurs
+Game::Game()
 {
-    std::cout << "Hello World!\n";
+	this->initVariables();
+	this->initWindow();
+	this->initEnemy();
+}
+// Destructeurs
+Game::~Game()
+{
+	delete this->window;
+}
+// Fonctions privées
+void Game::initVariables()
+{
+	this->window = nullptr;
+}
+void Game::initWindow()
+{
+	this->videoMode.height = 600;
+	this->videoMode.width = 800;
+	this->window = new sf::RenderWindow(this->videoMode, "Game", sf::Style::Titlebar | sf::Style::Close);
+	this->window->setFramerateLimit(144);
+}
+void Game::initEnemy()
+{
+	this->enemy.setPosition(10.f, 10.f);
+	this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+	this->enemy.setScale(sf::Vector2f(0.5f, 0.5f));
+	this->enemy.setFillColor(sf::Color::Cyan);
+	this->enemy.setOutlineColor(sf::Color::Green);
+	this->enemy.setOutlineThickness(1.f);
+}
+// Fonctions
+void Game::update()
+{
+	this->pollevent();
+	this->enemy.setPosition(sf::Mouse::getPosition(*this->window).x, sf::Mouse::getPosition(*this->window).y);
+	std::cout << "Postion souris :" << sf::Mouse::getPosition(*this->window).x << " " << sf::Mouse::getPosition(*this->window).y << std::endl;
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+void Game::render()
+{
+	/* - Efface les frames précedents
+	.  - Render les objets
+	.  - Affiche la fénetre
+	*/
+	this->window->clear();
+	this->window->draw(this->enemy);
+	this->window->display();
+}
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+void Game::pollevent()
+{
+	while (this->window->pollEvent(this->ev))
+	{
+		switch (this->ev.type)
+		{
+			case sf::Event::Closed:
+				this->window->close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+					this->window->close();
+				break;
+			default:
+				continue;
+		}
+	}
+}
+
+// Acesseurs
+bool Game::running() const
+{
+	return (this->window)->isOpen();
+}
